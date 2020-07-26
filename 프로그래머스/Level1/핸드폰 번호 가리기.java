5b3e78a8a019a76e4619284790eeaa7040003666
LEEJEEWON <60544994+leejiwon6315@users.noreply.github.com>
2020-07-26 20:21:04 +0900

    Create 핸드폰 번호 가리기.java

---
+++ 프로그래머스/Level1/핸드폰 번호 가리기.java
@@ -0,0 +1,12 @@
+class Solution {
+  public String solution(String phone_number) {
+      String answer = "";
+      int num = phone_number.length();
+      for(int i=0; i<num-4; i++){
+          answer += '*';
+      }
+      answer += phone_number.substring(num-4,num);
+      
+      return answer;
+  }
+}
