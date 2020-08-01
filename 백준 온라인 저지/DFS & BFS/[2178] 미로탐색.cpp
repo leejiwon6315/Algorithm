878c4f80909bd8e1c180ccb5a83a806998870fdb
LEEJEEWON <60544994+leejiwon6315@users.noreply.github.com>
2020-08-01 15:56:29 +0900

    Create [2178] 미로탐색.cpp

---
+++ 백준 온라인 저지/DFS & BFS/[2178] 미로탐색.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+
+int n, m;
+int map[101][101] = {0,};
+int check[101][101] = {0,};
+int dx[] = {0,1,0,-1};
+int dy[] = {1,0,-1,0};
+
+void Bfs(int x, int y){
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    check[x][y] = 1;
+    
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+        
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx>=0 && ny>=0 && nx<n && ny<m){
+                if(check[nx][ny]==0 && map[nx][ny]==1){
+                    q.push(make_pair(nx,ny));
+                    check[nx][ny] = check[x][y] + 1;
+                }
+            }
+        }
+    }
+}
+
+int main(){
+
+    cin >> n >> m;
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%1d", &map[i][j]);
+        }
+    }
+    
+    Bfs(0, 0);
+    
+    cout << check[n-1][m-1] << endl;
+    return 0;
+}
