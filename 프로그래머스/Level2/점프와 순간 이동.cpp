ceefd2557eff21468dc0d6d386322bab106d3d07
LEEJEEWON <leejiwon6315@naver.com>
2020-08-27 19:47:08 +0900

    Create 점프와 순간 이동.cpp

---
+++ 프로그래머스/Level2/점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    while(n>1){
+        if(n%2 == 1){
+            n -= 1;
+            n /= 2;
+            ans ++;
+        }
+        else{
+            n /= 2;
+        }
+    }
+    return ans+1;
+}
