179890acf1dec6dc692fabc0693e72a9dc9c7d28
LEEJEEWON <leejiwon6315@naver.com>
2020-10-16 10:29:41 +0900

    Create 완주하지 못한 선수.cpp

---
+++ 프로그래머스/Level1/완주하지 못한 선수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    for(int i=0; i<completion.size(); i++){
+        if(completion[i] != participant[i]){
+            return participant[i]; 
+        }
+    }
+    answer = participant[participant.size()-1];
+    return answer;
+}
