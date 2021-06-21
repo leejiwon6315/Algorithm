93f9100bc275892ced6149c2a16efd59c525a32c
LEEJEEWON <leejiwon6315@naver.com>
2021-06-21 20:34:19 +0900

    Create K번째수.js

---
+++ 프로그래머스/Level1/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    let answer = [];
+    
+    for(let i=0; i<commands.length; i++){
+        let tmp = array.slice(commands[i][0]-1, commands[i][1]);
+        tmp.sort((a,b) => a-b);
+        
+        answer.push(tmp[commands[i][2]-1]);
+    }
+    
+    return answer;
+}
