c67e6f7b80405a3a2da262c40c3b5e8a617033d3
LEEJEEWON <leejiwon6315@naver.com>
2021-07-28 14:16:55 +0900

    Update 소수 찾기.js

--- 프로그래머스/Level1/소수 찾기.js
+++ 프로그래머스/Level1/소수 찾기.js
@@ -1,16 +1,25 @@
+let PrimeArray = [];
+
+function Eratos(n){
+    for(let i=2; i<=n; i++)
+        PrimeArray[i] = true;
+
+    for(let i=2; i*i<=n; i++){
+        if(PrimeArray[i]){
+            for(let j=i*i; j<=n; j+=i)
+                PrimeArray[j] = false;
+        }
+    }
+}
+
 function solution(n) {
+    if(n === 2) return 1;
     let answer = 0;
     
-    function primeNum(number){
-        if(number === 2) return true;
-        for(let i=2; i<=Math.floor(Math.sqrt(number)); i++){
-            if(number%i === 0) return false;
-        }
-        return true;
-    }
-
-    for(let j=2; j<=n; j++)
-        if(primeNum(j)) answer ++;
+    Eratos(n);
+    
+    for(let i=2; i<=n+1; i++)
+        if(PrimeArray[i] == true) answer++;
     
     return answer;
 }
